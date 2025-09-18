835efe35b1fd5c51b36d3ac7e3335449fd6dd0a0
Jang Seung Hoon <wjang07@gmail.com>
2025-09-18 19:12:19 +0900

    🎨 chore: update icon component

--- src/components/commons/Icon.tsx
+++ src/components/commons/Icon.tsx
@@ -1,33 +1,36 @@
-import CommentIcon from "@/assets/icons/comment.svg";
-import FavoriteIcon from "@/assets/icons/favorite.svg";
+import CommentIcon from '@/assets/icons/comment.svg'
+import FavoriteIcon from '@/assets/icons/favorite.svg'
+import InstagramIcon from '@/assets/icons/socials/instagram.svg'
 
-export type IconTypes = "comment" | "favorite";
+export type IconTypes = 'comment' | 'favorite' | 'instagram'
 
 interface IconProps {
-  type: IconTypes;
-  size?: string;
-  color?: string;
+  type: IconTypes
+  color?: string
+  className?: string
 }
 
 function renderIcon(icon: IconTypes) {
   switch (icon) {
-    case "comment":
-      return <CommentIcon />;
-    case "favorite":
-      return <FavoriteIcon />;
+    case 'comment':
+      return <CommentIcon />
+    case 'favorite':
+      return <FavoriteIcon />
+    case 'instagram':
+      return <InstagramIcon />
   }
 }
 
 export default function Icon({
   type,
-  size = "h-6",
-  color = "text-gray-900",
+  color = 'text-gray-900',
+  className,
 }: IconProps) {
   return (
     <span
-      className={`inline-block align-middle fill-current [&_*]:fill-current ${size} ${color}`}
+      className={`inline-block fill-current align-middle [&_*]:fill-current ${color} ${className}`}
     >
       {renderIcon(type)}
     </span>
-  );
+  )
 }
