85650c912dd92aa65b478c9d3ebb151f29812425
Jang Seung Hoon <wjang07@gmail.com>
2025-09-19 21:44:11 +0900

    🔧chore: update svgr config

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    rules: {
-      "*.svg": {
-        loaders: ["@svgr/webpack"],
-        as: "*.js",
+  // TurboPack 설정
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
       },
     },
   },
-  webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule: any) =>
-      rule.test?.test?.(".svg")
-    );
+  // webpack 설정
+  webpack: (config) => {
+    // @ts-expect-error 타입 에러 무시
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    )
 
     config.module.rules.push(
       {
@@ -24,14 +29,20 @@ const nextConfig: NextConfig = {
         test: /\.svg$/i,
         issuer: fileLoaderRule.issuer,
         resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: ["@svgr/webpack"],
-      }
-    );
-
-    fileLoaderRule.exclude = /\.svg$/i;
-
-    return config;
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              typescript: true,
+              ext: 'tsx',
+            },
+          },
+        ],
+      },
+    )
+    fileLoaderRule.exclude = /\.svg$/i
+    return config
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
--- src/types/svgr.d.ts
+++ src/types/svgr.d.ts
@@ -1,10 +1,5 @@
 declare module '*.svg' {
-  import { FC, SVGProps } from 'react'
-  const content: FC<SVGProps<SVGElement>>
-  export default content
-}
-
-declare module '*.svg?url' {
-  const content: any
-  export default content
+  import React from 'react'
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>
+  export default SVG
 }
